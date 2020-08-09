01762eb3c2ec4eab22bfd829953f3516451d2e20
盛志文 <senwev@gmail.com>
2020-08-09 13:01:54 +0800

    更改：新增输出

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -7,6 +7,7 @@ namespace Demo1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("新增输出");
             Console.Read();
         }
     }
